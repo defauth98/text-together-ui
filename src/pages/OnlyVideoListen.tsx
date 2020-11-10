90a35cb5e678fa01efb83eda47d1a310418dc6fd
defauth98 <neto.daniribeiro@gmail.com>
2020-11-10 13:41:30 -0300

    Arrumando os playes do youtube

--- src/pages/OnlyVideoListen.tsx
+++ src/pages/OnlyVideoListen.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import YouTube from 'react-youtube';
 import NextButton from '../components/NextButton';
@@ -9,14 +9,26 @@ import 'dotenv/config';
 import { useVideoContext } from '../contexts/videoContext';
 
 function OnlyVideoListenPage() {
-  const { videoTitle, videoID } = useVideoContext();
+  const [videoId, setVideoId] = useState('');
+  const { videoTitle, videoLink } = useVideoContext();
+
+  useEffect(() => {
+    setVideoId(getVideoIdFromUrl(String(videoLink)));
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  function getVideoIdFromUrl(url: String) {
+    let regExp = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#&?]*).*/;
+    let match = url.match(regExp);
+    return match && match[7].length === 11 ? match[7] : '';
+  }
 
   return (
     <div id="only-video-listen-page">
       <h1>{videoTitle}</h1>
       <div className="video-container">
         <YouTube
-          videoId={videoID}
+          videoId={videoId}
           opts={{ height: '600px', width: '1200px' }}
         />
       </div>
--- src/pages/VideoListenWithText.tsx
+++ src/pages/VideoListenWithText.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import YouTube from 'react-youtube';
 
 import NextButton from '../components/NextButton';
@@ -6,7 +6,20 @@ import { useVideoContext } from '../contexts/videoContext';
 import '../styles/pages/video-listen-with-text-page.css';
 
 function VideoListenPageWithText() {
-  const { videoID, textEnglish, videoTitle } = useVideoContext();
+  const [videoIDstate, setVideoId] = useState('');
+
+  const { textEnglish, videoTitle, videoLink } = useVideoContext();
+
+  useEffect(() => {
+    setVideoId(getVideoIdFromUrl(String(videoLink)));
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  function getVideoIdFromUrl(url: String) {
+    let regExp = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#&?]*).*/;
+    let match = url.match(regExp);
+    return match && match[7].length === 11 ? match[7] : '';
+  }
 
   return (
     <div id="video-listen-with-text">
@@ -17,7 +30,7 @@ function VideoListenPageWithText() {
         </div>
         <div className="video-container">
           <YouTube
-            videoId={videoID}
+            videoId={videoIDstate}
             opts={{ height: '400px', width: '600px' }}
           />
         </div>
